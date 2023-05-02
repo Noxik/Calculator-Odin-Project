e071be10836544ea010bcfa69af303d1adca3709
Noxik <64279796+Noxik@users.noreply.github.com>
2023-05-02 18:02:39 +0200

    add Operator after click button with operator, choosen operator save as MATH parameter from operate function

--- scripts.js
+++ scripts.js
@@ -3,22 +3,52 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a/b;
 
-let second;
-let operator;
+let math
+let a;
+let b;
 
 const operate = (a, b, math) => math(a,b);
-let first = ""/* variable display */
+
+let show = ""/* variable display */
 let digit /* 0-9 calc */
 
 let result = document.querySelector(".result")
 
+
 /* button click to result div */
-const btn = document.querySelectorAll(".digit");
-btn.forEach( function(e) {
+const digitBtn = document.querySelectorAll(".digit");
+digitBtn.forEach( function(e) {
     e.addEventListener("click", () => {
         digit = e.textContent;
-        first += digit.toString() /* add to take more than 1 digit from calc */
-        result.innerText = first;
+        show += digit.toString() /* add to take more than 1 digit from calc */
+        result.innerText = show;
    })})
 
+/* button click choose operator => math var */
+const operatorBtn = document.querySelectorAll(".operator");
+operatorBtn.forEach( function(e) {
+          e.addEventListener("click", () => {
+            switch(e.textContent) {
+                case ("x"):
+                    math = multiply;
+                    break;
+                case ("+"):
+                    math = add;
+                    break; 
+                case ("-"):
+                    math = subtract;
+                    break; 
+                case ("/"):
+                    math = divide;
+                    break; 
+                }
+            console.log(e.textContent);
+            console.log(math)
+         })})
    
+
+let cleanC = document.querySelector(".clean")
+cleanC.addEventListener("click", () => {
+    result.innerText = "0";
+    show = ""
+})
